dbbb195e2dba58f6bacdef400332e21a438c4069
Nick Field <heyimnick@live.com>
2016-12-09 20:17:20 +0000

    add server file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const hapi = require('hapi');
+const server = require('./src/server.js');
+
+server.start((err) => {
+  if (err) { throw err; }
+  console.log(`Server is running at ${server.info.uri}`);
+});
---
+++ src/server.js
@@ -0,0 +1,53 @@
+const Hapi = require('hapi');
+const cookieAuth = require('hapi-auth-cookie');
+const hapiContextCredentials = require('hapi-context-credentials');
+const vision = require('vision');
+const inert = require('inert');
+const routes = require('./routes.js');
+const server = new Hapi.Server();
+const path = require('path');
+const env = require('env2');
+
+// Install environment variables
+env('./config.env');
+
+const options = {
+  password: process.env.COOKIE_PASSWORD,
+  cookie: 'somecookie',
+  ttl: 24 * 60 * 60 * 1000,
+  isSecure: process.env.NODE_ENV === 'PRODUCTION',
+  isHttpOnly: false
+};  // make this :)
+
+// Uncomment for production
+// setInterval(() => {
+//   require('http').get('http://the-badgerer.herokuapp.com');
+// }, 300000); // every 5 minutes
+
+server.connection({
+  port: process.env.PORT || 8080,
+  routes: {
+    files: {
+      relativeTo: path.join(__dirname, '../public')
+    }
+  }
+});
+
+server.register([vision, inert, cookieAuth, hapiContextCredentials], (err) => {
+  if (err) { throw err; }
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: '../views',
+    layoutPath: '../views/layout/',
+    helpersPath: '../views/helpers/',
+    layout: 'layout',
+    partialsPath: '../views/partials/'
+  });
+  server.auth.strategy('session', 'cookie', options);
+  server.route(routes);
+});
+
+module.exports = server;
