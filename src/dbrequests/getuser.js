7cfa5f1f22433ea254cb9c2f16bcd37e4b3c92e8
Nick Field <heyimnick@live.com>
2016-12-12 21:33:13 +0000

    add

--- src/dbrequests/getuser.js
+++ src/dbrequests/getuser.js
@@ -1,7 +1,7 @@
 const dbConn = require('../dbconnection.js');
 
 module.exports = (username, cb) => {
-  dbConn.query(`SELECT * FROM Owners WHERE OwnerUsername = $1`, [username], (err, data) => {
+  dbConn.query(`SELECT * FROM owners WHERE ownerusername = $1`, [username], (err, data) => {
     (err ? cb(err) : cb(null, data.rows[0]));
   });
 };
