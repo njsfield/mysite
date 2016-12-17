2bb53a4d718a8c2dcf66fc5af0341a5db3f0fd11
Nick Field <heyimnick@live.com>
2016-12-17 12:36:18 +0000

    add working gallery

--- public/js/imagepanel.js
+++ public/js/imagepanel.js
@@ -1,9 +1,34 @@
 (function () {
   var imageBtn = document.querySelector('.edit__image-button');
-  var bodyElt = document.querySelector('body');
+  var imagesContainer = document.querySelector('.images');
+  var gallery = document.querySelector('.images__container');
+  var backBtn = document.querySelector('.images__back');
+
   var XML = XMLHttpRequest;
 
-  // Display imageDiv
+  // Toggle Elt className
+  function toggleElt (elt, className) {
+    if (elt.className.indexOf(className) > -1) {
+      elt.classList.remove(className);
+    } else {
+      elt.classList.add(className);
+    }
+  }
+
+  // clearContent
+  function clearContent (elt) {
+    elt.innerHTML = '';
+  }
+
+  // Inject Images
+  function injectImages (images, elt, className) {
+    images.forEach(function (image) {
+      var imageElt = document.createElement('span');
+      imageElt.style.backgroundImage = 'url(/images/' + image + ')';
+      imageElt.className = className;
+      elt.appendChild(imageElt);
+    });
+  }
 
   // Make request
   function fetchImages (cb) {
@@ -18,8 +43,16 @@
   // Image Button
   imageBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    fetchImages(function (data) {
-      console.log(data);
+    toggleElt(imagesContainer, 'images--hidden');
+    clearContent(gallery);
+    fetchImages(function (images) {
+      injectImages(images, gallery, 'images__image');
     });
+  });
+
+  // Back Button
+  backBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    toggleElt(imagesContainer, 'images--hidden');
   });
 })();
