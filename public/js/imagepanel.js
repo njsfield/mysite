f2311b72fbd83070cdad475581585456116696d5
Nick Field <heyimnick@live.com>
2016-12-17 10:34:13 +0000

    add working button

---
+++ public/js/imagepanel.js
@@ -0,0 +1,7 @@
+(function () {
+  var imageBtn = document.querySelector('.edit__image-button');
+  imageBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('yes');
+  });
+})();
