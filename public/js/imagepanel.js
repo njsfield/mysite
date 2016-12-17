05f97c8355c56ca474e473e997b4535b73ff9b97
Nick Field <heyimnick@live.com>
2016-12-17 10:45:49 +0000

    image button returns array of images

--- public/js/imagepanel.js
+++ public/js/imagepanel.js
@@ -1,7 +1,25 @@
 (function () {
   var imageBtn = document.querySelector('.edit__image-button');
+  var bodyElt = document.querySelector('body');
+  var XML = XMLHttpRequest;
+
+  // Display imageDiv
+
+  // Make request
+  function fetchImages (cb) {
+    var xhr = new XML();
+    xhr.addEventListener('load', function (res) {
+      cb(JSON.parse(res.target.responseText).images);
+    });
+    xhr.open('get', '/images');
+    xhr.send();
+  }
+
+  // Image Button
   imageBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('yes');
+    fetchImages(function (data) {
+      console.log(data);
+    });
   });
 })();
