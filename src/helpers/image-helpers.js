f046d956d9fd68e3ed65f61082501ad0b7f32437
Nick Field <heyimnick@live.com>
2016-12-18 14:14:50 +0000

    refactor

---
+++ src/helpers/image-helpers.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+const readImages = (cb) => {
+  fs.readdir(path.join(__dirname, '../../public/images'), (err, images) => {
+    if (err) cb(err);
+    else cb(null, images);
+  });
+};
+
+const writeImage = (filename, data, cb) => {
+  fs.writeFile(path.join(__dirname, '../../public/images', filename), data, (err) => {
+    err ? cb(err) : cb(null);
+  });
+};
+
+const sanitizeImagePath = (uri) => {
+  uri = uri.replace(/\s/g, '-');
+  uri = uri.toLowerCase();
+  let images = fs.readdirSync(path.join(__dirname, '../../public/images'));
+
+  // Check for images
+  let imageFind = (uri) => {
+    return images.indexOf(uri) > -1;
+  };
+
+  // While found
+  while (imageFind(uri)) {
+    if (/\d(?=\.)/.test(uri)) {
+      uri = uri.replace(/\d(?=\.)/, (match) => { return `${++match}`; });
+    } else {
+      uri = uri.replace(/\./, '1.');
+    }
+  }
+  return uri;
+};
+
+const decodeBase64Image = (dataString) => {
+  const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+  const response = {};
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string');
+  }
+
+  response.type = matches[1];
+  response.data = new Buffer(matches[2], 'base64');
+
+  return response;
+};
+
+module.exports = {
+  readImages,
+  writeImage,
+  sanitizeImagePath,
+  decodeBase64Image
+};
--- src/routes/private/images.js
+++ src/routes/private/images.js
@@ -1,12 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-const decodeBase64Image = require('../../helpers/decode64baseimage.js');
+const {readImages, writeImage, decodeBase64Image, sanitizeImagePath} = require('../../helpers/image-helpers');
 const addImageToDb = require('../../dbrequests/addimage');
-
-const sanitizePath = (path) => {
-  path = path.replace(/\s/g, '-');
-  return path.toLowerCase();
-};
 
 module.exports = {
   path: '/images',
@@ -18,16 +11,15 @@ module.exports = {
     },
     handler: (req, reply) => {
       if (req.method === 'get') {
-        fs.readdir(path.join(__dirname, '../../../public/images'), (err, images) => {
-          if (err) throw err;
-          reply({images: images});
+        readImages((err, images) => {
+          err ? reply(err) : reply({images: images});
         });
       } else {
-        let imageBuffer = decodeBase64Image(req.payload);
-        let imageName = sanitizePath(req.query.name);
-        fs.writeFile(path.join(__dirname, '../../../public/images', imageName), imageBuffer.data, (err) => {
+        let imageData = decodeBase64Image(req.payload);
+        let fileName = sanitizeImagePath(req.query.name);
+        writeImage(fileName, imageData.data, (err) => {
           if (err) throw err;
-          addImageToDb(imageName, (err) => {
+          addImageToDb(fileName, (err) => {
             if (err) throw err;
             reply('done');
           });
