859886467667877c6d2ce4237097a18cb422ed53
Nick Field <heyimnick@live.com>
2016-12-18 09:41:33 +0000

    add marked list styles

--- src/helpers/markdowntransform.js
+++ src/helpers/markdowntransform.js
@@ -1,4 +1,6 @@
 const marked = require('marked');
+const renderer = new marked.Renderer();
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   gfm: true,
@@ -10,4 +12,18 @@ marked.setOptions({
   smartypants: false
 });
 
-module.exports = (text) => marked(text);
+renderer.heading = (text, level) => {
+  let escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+  return `<h${level} id="${escapedText}"class="title">${text}</h${level}>`;
+};
+
+renderer.list = (body, boolean) => {
+  return `<${boolean ? 'ol' : 'ul'} class="${boolean ? 'ol' : 'ul'}">${body}</${boolean ? 'ol' : 'ul'}>`;
+};
+
+renderer.listitem = (text) => {
+  return `<li class="li">${text}</li>`;
+};
+
+module.exports = (text) => marked(text, {renderer: renderer});
