3be8b1defa543222c7f3222cce5361a448a44b97
Nick Field <heyimnick@live.com>
2016-12-22 17:24:11 +0000

    add portfolio hover class

--- views/helpers/categorycolor.js
+++ views/helpers/categorycolor.js
@@ -5,6 +5,10 @@ module.exports = (categoryname) => {
     case 'design' : { color = 'pink'; break; }
     case 'javascript' : { color = 'red'; break; }
     case 'portfolio' : { color = 'green'; break; }
+    case 'databases' : { color = 'grey'; break; }
+    case 'testing' : { color = 'purple'; break; }
+    case 'html' : { color = 'orange'; break; }
+    case 'personal' : { color = 'light-blue'; break; }
   }
   return color;
 };
