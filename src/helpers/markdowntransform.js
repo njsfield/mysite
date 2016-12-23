f733754c65fcc9f738bec6fe49ffdb63b6adb356
Nick Field <heyimnick@live.com>
2016-12-23 17:50:53 +0000

    add markdown styles

--- src/helpers/markdowntransform.js
+++ src/helpers/markdowntransform.js
@@ -43,16 +43,44 @@ renderer.link = (href, title, text) => {
 
 /* Heading */
 renderer.heading = (text, level) => {
-  return `<h${level} class="h${level}">${text}</h${level}>`;
+  const align = /^{.+}$/.test(text) ? `h${level}--center` : '';
+  text = align ? text.split('').slice(1, -1).join('') : text;
+  return `<h${level} class="h${level} ${align}">${text}</h${level}>`;
 };
 
 /* List */
 renderer.list = (body, boolean) => {
   return `<${boolean ? 'ol' : 'ul'} class="${boolean ? 'ol' : 'ul'}">${body}</${boolean ? 'ol' : 'ul'}>`;
 };
-
+/* List Item */
 renderer.listitem = (text) => {
   return `<li class="li">${text}</li>`;
+};
+
+/* Table */
+renderer.table = (thead, tbody) => {
+  return `<table class="table">
+    <thead class="table__thead">${thead}</thead>
+    <tbody class="table__tbody">${tbody}</tbody>
+    </table>`;
+};
+
+/* Table Row */
+renderer.tablerow = (tr) => {
+  return `<tr class="table__tr">${tr}</tr>`;
+};
+
+/* Table Row */
+renderer.tablecell = (td, flags) => {
+  const type = flags.header ? 'th' : 'td';
+  const align = flags.align || 'left';
+  const tdAlignClass = flags.header ? '' : `table__td--${align}`;
+  return `<${type} class="table__${type} ${tdAlignClass}">${td}</${type}>`;
+};
+
+/* Block Quote */
+renderer.blockquote = (quote) => {
+  return `<div class="blockquote">${quote}</blockquote>`;
 };
 
 module.exports = (text) => marked(text, {renderer: renderer});
