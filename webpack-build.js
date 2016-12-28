05800574fe66beb7a5c2ce711d5691850f309d08
Nick Field <heyimnick@live.com>
2016-12-28 12:36:49 +0000

    convert css to sass, bundle sass file to style.css. Remove existing directories in public/

--- webpack-build.js
+++ webpack-build.js
@@ -1,4 +1,4 @@
 // Fetches base sass file to build style.css file to /public
-// require('./src/css/index.scss');
+require('./src/css/main.scss');
 // Fetches base js file to build app.js file to /public
 require('./src/js/main.js');
